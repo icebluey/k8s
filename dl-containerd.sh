869820439c9a00e9e515eb546f3106533fe87bc0
Kenneth Davidson <xylene2016@protonmail.com>
2024-09-15 11:20:29 -0400

    Update dl-containerd.sh

--- dl-containerd.sh
+++ dl-containerd.sh
@@ -19,8 +19,13 @@ _install_go() {
     #_go_version="$(wget -qO- 'https://golang.org/dl/' | grep -i 'linux-amd64\.tar\.' | sed 's/"/\n/g' | grep -i 'linux-amd64\.tar\.' | cut -d/ -f3 | grep -i '\.gz$' | sed 's/go//g; s/.linux-amd64.tar.gz//g' | grep -ivE 'alpha|beta|rc' | sort -V | uniq | grep '^1\.18\.' | tail -n 1)"
     #wget -q -c -t 0 -T 9 "https://dl.google.com/go/go${_go_version}.linux-amd64.tar.gz"
     # Go1.20.X
-    _go_version="$(wget -qO- 'https://golang.org/dl/' | grep -i 'linux-amd64\.tar\.' | sed 's/"/\n/g' | grep -i 'linux-amd64\.tar\.' | cut -d/ -f3 | grep -i '\.gz$' | sed 's/go//g; s/.linux-amd64.tar.gz//g' | grep -ivE 'alpha|beta|rc' | sort -V | uniq | grep '^1\.20\.' | tail -n 1)"
+    #_go_version="$(wget -qO- 'https://golang.org/dl/' | grep -i 'linux-amd64\.tar\.' | sed 's/"/\n/g' | grep -i 'linux-amd64\.tar\.' | cut -d/ -f3 | grep -i '\.gz$' | sed 's/go//g; s/.linux-amd64.tar.gz//g' | grep -ivE 'alpha|beta|rc' | sort -V | uniq | grep '^1\.20\.' | tail -n 1)"
+    #wget -q -c -t 0 -T 9 "https://dl.google.com/go/go${_go_version}.linux-amd64.tar.gz"
+
+    # Go1.22.X
+    _go_version="$(wget -qO- 'https://golang.org/dl/' | grep -i 'linux-amd64\.tar\.' | sed 's/"/\n/g' | grep -i 'linux-amd64\.tar\.' | cut -d/ -f3 | grep -i '\.gz$' | sed 's/go//g; s/.linux-amd64.tar.gz//g' | grep -ivE 'alpha|beta|rc' | sort -V | uniq | grep '^1\.22\.' | tail -n 1)"
     wget -q -c -t 0 -T 9 "https://dl.google.com/go/go${_go_version}.linux-amd64.tar.gz"
+
     rm -fr /usr/local/go
     sleep 1
     install -m 0755 -d /usr/local/go
@@ -41,7 +46,80 @@ _install_go() {
     go version
     echo
 }
-_install_go
+
+_install_libseccomp() {
+    set -e
+    _tmp_dir="$(mktemp -d)"
+    cd "${_tmp_dir}"
+    wget -c -t 9 -T 9 "https://github.com/seccomp/libseccomp/releases/download/v2.5.5/libseccomp-2.5.5.tar.gz"
+    tar -xof libseccomp-*.tar*
+    sleep 1
+    rm -f libseccomp-*.tar*
+    cd libseccomp-*
+    ./configure \
+    --build=x86_64-linux-gnu \
+    --host=x86_64-linux-gnu \
+    --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin \
+    --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include \
+    --libdir=/usr/lib/x86_64-linux-gnu --libexecdir=/usr/libexec --localstatedir=/var \
+    --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info \
+    --enable-shared --enable-static
+    make all
+    rm -fr /tmp/libseccomp
+    make DESTDIR=/tmp/libseccomp install
+    cd /tmp/libseccomp
+    if [[ "$(pwd)" = '/' ]]; then
+        echo
+        printf '\e[01;31m%s\e[m\n' "Current dir is '/'"
+        printf '\e[01;31m%s\e[m\n' "quit"
+        echo
+        exit 1
+    else
+        rm -fr lib64
+        rm -fr lib
+        chown -R root:root ./
+    fi
+    find usr/ -type f -iname '*.la' -delete
+    if [[ -d usr/share/man ]]; then
+        find -L usr/share/man/ -type l -exec rm -f '{}' \;
+        sleep 2
+        find usr/share/man/ -type f -iname '*.[1-9]' -exec gzip -f -9 '{}' \;
+        sleep 2
+        find -L usr/share/man/ -type l | while read file; do ln -svf "$(readlink -s "${file}").gz" "${file}.gz" ; done
+        sleep 2
+        find -L usr/share/man/ -type l -exec rm -f '{}' \;
+    fi
+    if [[ -d usr/lib/x86_64-linux-gnu ]]; then
+        find usr/lib/x86_64-linux-gnu/ -type f \( -iname '*.so' -or -iname '*.so.*' \) | xargs --no-run-if-empty -I '{}' chmod 0755 '{}'
+        find usr/lib/x86_64-linux-gnu/ -iname 'lib*.so*' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+        find usr/lib/x86_64-linux-gnu/ -iname '*.so' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    if [[ -d usr/lib64 ]]; then
+        find usr/lib64/ -type f \( -iname '*.so' -or -iname '*.so.*' \) | xargs --no-run-if-empty -I '{}' chmod 0755 '{}'
+        find usr/lib64/ -iname 'lib*.so*' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+        find usr/lib64/ -iname '*.so' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    if [[ -d usr/sbin ]]; then
+        find usr/sbin/ -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    if [[ -d usr/bin ]]; then
+        find usr/bin/ -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    echo
+    rm -vf /usr/lib/x86_64-linux-gnu/libseccomp.a
+    rm -vf /usr/lib/x86_64-linux-gnu/libseccomp.so.2.5.[1234]
+    sleep 2
+    /bin/cp -afr * /
+    sleep 2
+    cd /tmp
+    rm -fr "${_tmp_dir}"
+    rm -fr /tmp/libseccomp
+    /sbin/ldconfig
+}
+
+set -e
+
+_install_libseccomp
 
 # Go programming language
 export GOROOT='/usr/local/go'
@@ -52,8 +130,7 @@ export PATH="$GOROOT/bin:$PATH"
 alias go="$GOROOT/bin/go"
 alias gofmt="$GOROOT/bin/gofmt"
 rm -fr ~/.cache/go-build
-
-set -e
+_install_go
 
 _tmp_dir="$(mktemp -d)"
 cd "${_tmp_dir}"
@@ -201,4 +278,3 @@ echo
 echo " package containerd ${_containerd_ver} done"
 echo
 exit
-
